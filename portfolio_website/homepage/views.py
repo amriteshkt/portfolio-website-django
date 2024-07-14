c1740fe52d8778a2a30ec2546dbf7a4e0495c0b7
amriteshkt <165368948+amriteshkt@users.noreply.github.com>
2024-07-14 18:37:25 +0530

    Update views.py

--- portfolio_website/homepage/views.py
+++ portfolio_website/homepage/views.py
@@ -8,4 +8,10 @@ def projects(request):
     return render(request, 'projects.html')
 
 def contact_me(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        description = request.POST.get("description")
+        # print(f"Hi! {name}, thank you for contacting me!")
     return render(request, 'contactme.html')
